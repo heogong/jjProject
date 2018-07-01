5e8169f72f91332ff8e99162b7ae4b70efff3b6b
heogong <heogong@gmail.com>
2018-07-01 21:15:01 +0900

    TAB 기능 이상 있음

--- frontend/src/components/TEACHER/Info.vue
+++ frontend/src/components/TEACHER/Info.vue
@@ -3,13 +3,18 @@
     <v-layout row wrap>
       <v-flex xs12>
         <v-tabs left color="teal darken-4" slider-color="teal">
+
           <v-tab v-for="n in 3" :key="n"> 
             Item {{ n }} 
           </v-tab>
-          <v-tab-item v-for="n in 3" :key="n">
+          <!-- <v-tab-item v-for="n in 3" :key="n">
             <TeacherView v-if="n == 1" v-bind:parentData="userId"></TeacherView>
             <TeacherSchedule v-if="n == 2" v-bind:parentData="userId"></TeacherSchedule>
+          </v-tab-item> -->
+          <v-tab-item>
+            <TeacherView v-bind:parentData="userId"></TeacherView>
           </v-tab-item>
+
         </v-tabs>
       </v-flex>
     </v-layout>
--- frontend/src/components/TEACHER/Schedule.vue
+++ frontend/src/components/TEACHER/Schedule.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>스케쥴</h1>
+  <h1>스케쥴!!!!!</h1>
 </template>
 
 <script>
