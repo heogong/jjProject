c8fcee4bb61deacd43faba1cc7fe48e769e8fb83
DESKTOP-ASVRCK1\JJ <heogong79@gmail.com>
2018-07-05 18:07:58 +0900

    캘린더

--- frontend/src/components/TEACHER/Info.vue
+++ frontend/src/components/TEACHER/Info.vue
@@ -3,14 +3,23 @@
     <v-layout row wrap>
       <v-flex xs12>
         <v-tabs left color="teal darken-4" slider-color="teal">
-          <v-tab v-for="n in 3" :key="n"> 
+          <!-- <v-tab v-for="n in 3" :key="n" > 
             Item {{ n }} 
+          </v-tab> -->
+          <v-tab> 
+            View
+          </v-tab>
+          <v-tab @click="scheduleClick"> 
+            Schedule
+          </v-tab>
+          <v-tab> 
+            Etc
           </v-tab>
           <v-tab-item>
             <TeacherView v-bind:parentData="userId"></TeacherView>
           </v-tab-item>
           <v-tab-item>
-            <TeacherSchedule v-bind:parentData="userId"></TeacherSchedule>
+            <TeacherSchedule v-bind:parentData="[userId, openClick]"></TeacherSchedule>
           </v-tab-item>
         </v-tabs>
       </v-flex>
@@ -30,11 +39,17 @@ export default {
   },
   data () {
     return {
-      userId : ''
+      userId : '',
+      openClick : false
     }
   },
   created : function() {
     this.userId = this.$route.params.userId
+  },
+  methods : {
+    scheduleClick : function() {
+      this.openClick = true;
+    }
   }
 }
 </script>
--- frontend/src/components/TEACHER/Schedule.vue
+++ frontend/src/components/TEACHER/Schedule.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="padding-top:20px">
     <div id='calendar'></div>
   </div>
 </template>
@@ -7,81 +7,96 @@
 <script>
 import $ from 'jquery';
 import 'fullcalendar';
-import 'fullcalendar/dist/fullcalendar.min.css' // Ensure you are using css-loader
+import 'fullcalendar/dist/fullcalendar.min.css'
 
 export default {
   name: 'Teacher_Schedule',
+  props: ['parentData'],
   mounted : function() {
-    //console.log(this.parentData,'sch-Userid');
-    var containerEl = $('#calendar');
+    //console.log(this.parentData[0],'sch-Userid');
+    //console.log(this.parentData[1],'openClick');
+  },
+  methods : {
+    setSchedule : function() {
+      var containerEl = $('#calendar');
 
-    containerEl.fullCalendar({
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay,listWeek'
-      },
-      defaultDate: '2018-01-12',
-      navLinks: true, // can click day/week names to navigate views
-      editable: true,
-      eventLimit: true, // allow "more" link when too many events
-      events: [
-        {
-          title: 'All Day Event',
-          start: '2018-01-01',
+      containerEl.fullCalendar({
+        locale : 'ko',
+        height : 730,
+        header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'month,agendaWeek,agendaDay,listWeek'
         },
-        {
-          title: 'Long Event',
-          start: '2018-01-07',
-          end: '2018-01-10'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: '2018-01-09T16:00:00'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: '2018-01-16T16:00:00'
-        },
-        {
-          title: 'Conference',
-          start: '2018-01-11',
-          end: '2018-01-13'
-        },
-        {
-          title: 'Meeting',
-          start: '2018-01-12T10:30:00',
-          end: '2018-01-12T12:30:00'
-        },
-        {
-          title: 'Lunch',
-          start: '2018-01-12T12:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: '2018-01-12T14:30:00'
-        },
-        {
-          title: 'Happy Hour',
-          start: '2018-01-12T17:30:00'
-        },
-        {
-          title: 'Dinner',
-          start: '2018-01-12T20:00:00'
-        },
-        {
-          title: 'Birthday Party',
-          start: '2018-01-13T07:00:00'
-        },
-        {
-          title: 'Click for Google',
-          url: 'http://google.com/',
-          start: '2018-01-28'
-        }
-      ]
-    })
+        defaultDate: '2018-01-12',
+        navLinks: true, // can click day/week names to navigate views
+        editable: true,
+        eventLimit: true, // allow "more" link when too many events
+        events: [
+          {
+            title: 'All Day Event',
+            start: '2018-01-01',
+          },
+          {
+            title: 'Long Event',
+            start: '2018-01-07',
+            end: '2018-01-10'
+          },
+          {
+            id: 999,
+            title: 'Repeating Event',
+            start: '2018-01-09T16:00:00'
+          },
+          {
+            id: 999,
+            title: 'Repeating Event',
+            start: '2018-01-16T16:00:00'
+          },
+          {
+            title: 'Conference',
+            start: '2018-01-11',
+            end: '2018-01-13'
+          },
+          {
+            title: 'Meeting',
+            start: '2018-01-12T10:30:00',
+            end: '2018-01-12T12:30:00'
+          },
+          {
+            title: 'Lunch',
+            start: '2018-01-12T12:00:00'
+          },
+          {
+            title: 'Meeting',
+            start: '2018-01-12T14:30:00'
+          },
+          {
+            title: 'Happy Hour',
+            start: '2018-01-12T17:30:00'
+          },
+          {
+            title: 'Dinner',
+            start: '2018-01-12T20:00:00'
+          },
+          {
+            title: 'Birthday Party',
+            start: '2018-01-13T07:00:00'
+          },
+          {
+            title: 'Click for Google',
+            url: 'http://google.com/',
+            start: '2018-01-28'
+          }
+        ]
+      })
+    }
+  },
+  watch: {
+    parentData: function(val) { // 스케쥴 탭 클릭 시 값 체크
+      if(val[1]) {
+        this.setSchedule();
+      }
+    }
   }
 }
 </script>
