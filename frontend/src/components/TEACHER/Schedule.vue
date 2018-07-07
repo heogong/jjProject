021608ab7fb4df9f9a4347a018809ebab35112d6
heogong <heogong@gmail.com>
2018-07-08 01:10:55 +0900

    토,일요일 색

--- frontend/src/components/TEACHER/Schedule.vue
+++ frontend/src/components/TEACHER/Schedule.vue
@@ -171,4 +171,14 @@ a {
 .fc-unthemed .fc-list-empty {
   background-color: #424242;
 }
+
+/* 토요일 */
+.fc-day-top.fc-sat.fc-past a { 
+  color:#0000FF; 
+}
+
+/* 일요일 */
+.fc-day-top.fc-sun.fc-past a { 
+  color:#FF0000; 
+}
 </style>
